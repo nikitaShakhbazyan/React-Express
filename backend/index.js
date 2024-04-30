5c291b3834f00afb9f925176451a047b27165899
nickshakh <shakhbazyann@gmail.com>
2024-04-30 18:38:24 +0400

    Added routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
+const routes = require('./routes/routes')
+
+app.use('/data',routes)
 
 const PORT = process.env.PORT || 3501
 
---
+++ backend/routes/routes.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+
+router.route('/').get((req,res) => {
+    res.status(200).json({message : "Get all contacts"})
+})
+
+router.route('/').post((req,res) => {
+    res.status(200).json({message : 'Create new contact'})
+})
+
+router.route('/:id').get((req,res) => {
+    res.status(200).json({message : `Get contact for ${req.params.id}`})
+})
+
+router.route('/:id').put((req,res) => {
+    res.status(200).json({message : `Update contact ${req.params.id}`})
+})
+
+
+router.route('/:id').delete((req,res) => {
+    res.status(200).json({message : `Delete contact ${req.params.id}`})
+})
+
+
+
+
+module.exports = router
