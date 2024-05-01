dae2fe6a5469aec8a10aaf49787a36e1c026fcad
nickshakh <shakhbazyann@gmail.com>
2024-05-01 13:03:02 +0400

    Added CORS

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
 const routes = require('./routes/routes')
-// const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use('/gifts',routes)
-// app.use(errorHandler)
 
 const PORT = process.env.PORT || 3501
 
